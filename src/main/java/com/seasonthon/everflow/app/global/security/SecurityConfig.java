8effc8efd8d9b76f0f7d8dbb6294d411b33c10eb
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-09-07 03:56:29 +0900

    Update allowed origins in CORS configuration

--- src/main/java/com/seasonthon/everflow/app/global/security/SecurityConfig.java
+++ src/main/java/com/seasonthon/everflow/app/global/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "https://everflow-frontend.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "https://everflowfront.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "RefreshToken", "id_token", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "RefreshToken"));
