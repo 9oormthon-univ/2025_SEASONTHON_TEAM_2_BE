c057739263f1300e70c9e7ca89d82c2c57c35b95
2ivii <dbswjdals2580@gmail.com>
2025-09-06 14:59:16 +0900

    feat#16 : 가족관련 알림 등록

--- src/main/java/com/seasonthon/everflow/app/family/service/FamilyService.java
+++ src/main/java/com/seasonthon/everflow/app/family/service/FamilyService.java
@@ -10,6 +10,8 @@ import com.seasonthon.everflow.app.family.dto.FamilyVerificationResponseDto;
 import com.seasonthon.everflow.app.family.repository.FamilyRepository;
 import com.seasonthon.everflow.app.global.code.status.ErrorStatus;
 import com.seasonthon.everflow.app.global.exception.GeneralException;
+import com.seasonthon.everflow.app.notification.domain.NotificationType;
+import com.seasonthon.everflow.app.notification.service.NotificationService;
 import com.seasonthon.everflow.app.user.domain.RoleType;
 import com.seasonthon.everflow.app.user.domain.User;
 import com.seasonthon.everflow.app.user.repository.UserRepository;
@@ -26,6 +28,7 @@ public class FamilyService {
 
     private final FamilyRepository familyRepository;
     private final UserRepository userRepository;
+    private final NotificationService notificationService;
 
     public void createFamily(Long userId, FamilyCreateRequestDto request) {
         User user = userRepository.findById(userId)
@@ -73,6 +76,7 @@ public class FamilyService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
 
+
         if (user.getRoleType() != RoleType.ROLE_GUEST) {
             throw new GeneralException(ErrorStatus.FAMILY_ALREADY_EXISTS);
         }
@@ -80,12 +84,24 @@ public class FamilyService {
         Family family = familyRepository.findByInviteCode(request.getInviteCode())
                 .orElseThrow(() -> new GeneralException(ErrorStatus.FAMILY_NOT_FOUND));
 
+        List<User> existingMembers = userRepository.findAllByFamilyId(family.getId());
+
         if (!family.getVerificationAnswer().equals(request.getVerificationAnswer())) {
             user.increaseFamilyJoinAttempts();
             userRepository.save(user);
 
             if (user.getFamilyJoinAttempts() >= 3) {
                 // TODO: 3회 이상 실패 시, 가입 대기 상태로 전환하고 알림을 보내는 등의 로직 추가
+                String link = "";
+                String contentText = String.format("%s님이 " + user.getFamily().getFamilyName()+ "에 참여를 요청했습니다.", user.getNickname());
+
+                existingMembers.forEach(recipient -> notificationService.sendNotification(
+                        recipient,
+                        NotificationType.FAMILY_ACTION,
+                        contentText,
+                        link
+                ));
+
                 throw new GeneralException(ErrorStatus.FAMILY_JOIN_FAILED);
             }
             throw new GeneralException(ErrorStatus.INVALID_VERIFICATION_ANSWER);
@@ -94,6 +110,17 @@ public class FamilyService {
         family.addMember(user);
         user.updateRole(RoleType.ROLE_USER);
         user.resetFamilyJoinAttempts();
+
+        String link = "";
+        String contentText = String.format("%s님이 " + user.getFamily().getFamilyName()+ "에 입장했어요.", user.getNickname());
+
+        // 5. 가족 구성원들에게 알림을 발송합니다 (답변자 본인 제외).
+        existingMembers.forEach(recipient -> notificationService.sendNotification(
+                recipient,
+                NotificationType.FAMILY_RESPONSE,
+                contentText,
+                link
+        ));
 
         familyRepository.save(family);
     }
