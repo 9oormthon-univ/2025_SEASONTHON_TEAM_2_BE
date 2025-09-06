01b39500fb7a7687d1c033bee61099be71d841e5
2ivii <dbswjdals2580@gmail.com>
2025-09-07 06:30:10 +0900

    refactor#24 : 약속액션수행시 read로 변경

--- src/main/java/com/seasonthon/everflow/app/notification/repository/NotificationRepository.java
+++ src/main/java/com/seasonthon/everflow/app/notification/repository/NotificationRepository.java
@@ -9,10 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findAllByUserIdAndReadStatusOrderByCreatedAtDesc(Long userId, ReadStatus readStatus);
     List<Notification> findTop3ByUserIdAndReadStatusOrderByCreatedAtDesc(Long userId, ReadStatus readStatus);
+
+    // 새롭게 추가된 코드
+    Optional<Notification> findByUserIdAndLinkAndNotificationType(Long userId, String link, NotificationType notificationType);
 
     @Modifying
     @Query("UPDATE Notification n SET n.readStatus = :status " +
