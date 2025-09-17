321f1a88ba1e032380c3e5daeeb650c8c6d0cc77
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-09-18 00:50:01 +0900

    fix#5 가족 API 오류 수정 메모 관련으로 가족 생성 안되는 버그 수정, 탈퇴 안되는 버그 수정

--- src/main/java/com/seasonthon/everflow/app/bookshelf/repository/BookshelfAnswerRepository.java
+++ src/main/java/com/seasonthon/everflow/app/bookshelf/repository/BookshelfAnswerRepository.java
@@ -1,10 +1,13 @@
 package com.seasonthon.everflow.app.bookshelf.repository;
 
 import com.seasonthon.everflow.app.bookshelf.domain.BookshelfAnswer;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookshelfAnswerRepository extends JpaRepository<BookshelfAnswer, Long> {
 
@@ -15,4 +18,8 @@ public interface BookshelfAnswerRepository extends JpaRepository<BookshelfAnswer
     long deleteByQuestionId(Long questionId);
 
     void deleteAllByUserId(Long userId);
+
+    @Modifying
+    @Query("delete from BookshelfAnswer ba where ba.question.createdBy.id = :userId")
+    void deleteAllByQuestionCreatedById(@Param("userId") Long userId);
 }
